7334f8ecae5cbfd804d7b4a3587e175c3bb588db
Björn Svensson <bjorn.svensson@ics.lu.se>
2023-11-06 09:12:10 +0100

    Added Employee boilerplate

---
+++ src/se/lu/ics/models/Employee.java
@@ -0,0 +1,27 @@
+package se.lu.ics.models;
+
+public class Employee {
+    private String employeeId;
+    private String name;
+
+    public Employee(String employeeId, String name) {
+        this.employeeId = employeeId;
+        this.name = name;
+    }
+
+    public String getEmployeeId() {
+        return this.employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
