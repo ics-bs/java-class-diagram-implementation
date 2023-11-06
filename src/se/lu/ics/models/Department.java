f7afdc61f27b63d02a124ae6d0a6fbae14d5413b
Björn Svensson <bjorn.svensson@ics.lu.se>
2023-11-06 09:12:25 +0100

    Added Department  boilerplate

--- src/se/lu/ics/models/Department.java
+++ src/se/lu/ics/models/Department.java
@@ -1,5 +1,30 @@
 package se.lu.ics.models;
 
 public class Department {
+    private String name;
+    private int budget;
+
+    public Department() {}
+
+    public Department(String name, int budget) {
+        this.name = name;
+        this.budget = budget;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBudget() {
+        return this.budget;
+    }
+
+    public void setBudget(int budget) {
+        this.budget = budget;
+    }
     
 }
