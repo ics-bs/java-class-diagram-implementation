4d63c10059239abbd69f399982ce298337302660
Björn Svensson <bjorn.svensson@ics.lu.se>
2023-11-06 09:16:16 +0100

    Added Employee one-to-many unary

--- src/se/lu/ics/models/Employee.java
+++ src/se/lu/ics/models/Employee.java
@@ -1,12 +1,35 @@
 package se.lu.ics.models;
 
+import java.util.ArrayList;
+
 public class Employee {
     private String employeeId;
     private String name;
 
+    // One-to-many unary association
+    private Employee manager;
+    private ArrayList<Employee> subordinates;
+
     public Employee(String employeeId, String name) {
         this.employeeId = employeeId;
         this.name = name;
+        subordinates = new ArrayList<Employee>();
+    }
+
+    public void addSubordinate(Employee employee) {
+        this.subordinates.add(employee);
+    }
+
+    public Employee getManager() {
+        return manager;
+    }
+
+    public void setManager(Employee manager) {
+        this.manager = manager;
+    }
+
+    public ArrayList<Employee> getSubordinates() {
+        return subordinates;
     }
 
     public String getEmployeeId() {
